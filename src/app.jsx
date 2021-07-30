387d8b0e8e1888f5f6d2da61bb8f5ba28d6c5382
skfotakf <blueland1998@naver.com>
2021-07-30 10:18:47 +0900

    useReducer 하고 있습니다

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,7 @@ import TodoList from './components/todoList';
 
 import TodoCreate from './components/todoCreate';
 import {createGlobalStyle} from 'styled-components';
+import { TodoProvider } from './components/todoContext';
 
 
 const GlobalStyle = createGlobalStyle`
@@ -17,13 +18,15 @@ const GlobalStyle = createGlobalStyle`
 function App() {
   return (
     <>
-      <GlobalStyle />
-      
-      <TodoTemplate>
-        <TodoHead />
-        <TodoList />
-        <TodoCreate />
-      </TodoTemplate>
+      <TodoProvider>
+        <GlobalStyle />
+        
+        <TodoTemplate>
+          <TodoHead />
+          <TodoList />
+          <TodoCreate />
+        </TodoTemplate>
+      </TodoProvider>
     </>
   );
 }
